5cec4205a1df3eea64676844c57ccd668bb3c76e
Jonatandb <jonatandb@gmail.com>
2022-04-27 11:56:41 -0300

    Remix and creating new posts
    https://daily-dev-tips.com/posts/remix-and-creating-new-posts/

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -27,6 +27,14 @@ export default function Posts() {
           </li>
         ))}
       </ul>
+      <p className='text-right'>
+        <Link
+          to="/posts/new"
+          className='rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300'
+        >
+          Create Post
+        </Link>
+      </p>
     </main>
   );
 }
