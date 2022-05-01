8e412a09a6f7caba00119fe7b79a83e9c294f19f
Jonatandb <jonatandb@gmail.com>
2022-05-01 12:10:35 -0300

    Some buttons added

--- app/routes/__auth/login.tsx
+++ app/routes/__auth/login.tsx
@@ -1,3 +1,16 @@
+import { Link } from "@remix-run/react";
+
 export default function Login() {
-  return <p>This is the login page</p>;
+  return <>
+    <p className="pb-4">This is the login page</p>
+
+    <Link
+      className="rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
+      to={{
+        pathname: "/register"
+      }}
+    >
+      Register
+    </Link>
+  </>
 }
--- app/routes/__auth/register.tsx
+++ app/routes/__auth/register.tsx
@@ -1,3 +1,15 @@
+import { Link } from "@remix-run/react";
+
 export default function Register() {
-  return <p>This is the register page</p>;
+  return <>
+    <p className="pb-4">This is the register page</p>
+    <Link
+      className="rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
+      to={{
+        pathname: "/login"
+      }}
+    >
+      Login
+    </Link>
+  </>
 }
