a3647a22d092554c8dcf1f4719c39406176d94d3
Jonatandb <jonatandb@gmail.com>
2022-05-07 22:25:07 -0300

    Remix adding resources with the link tag
    https://daily-dev-tips.com/posts/remix-adding-resources-with-the-link-tag/

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,9 +1,24 @@
 import { Link, Outlet } from '@remix-run/react';
+import type { LinksFunction } from '@remix-run/server-runtime';
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: 'icon',
+      href: '/Jonatandb_favicon.ico',
+      type: 'x-image/ico'
+    },
+    {
+      rel: 'stylesheet',
+      href: '/Jonatandb.css'
+    }
+  ];
+};
 
 export default function AdminAdmin() {
   return (
     <div className='mx-auto max-w-4xl'>
-      <h1 className='my-6 mb-2 border-b-2 text-center text-3xl'>
+      <h1 className='my-6 mb-2 border-b-2 text-center text-3xl jonatandb-red'>
         My super cool admin
       </h1>
       <header className='border-b-2 mb-2 p-2'>
